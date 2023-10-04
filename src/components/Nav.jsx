90c7c4198aa3884eb7ff61060b0cc3a1ac58925d
Amos <amossullivan23@gmail.com>
2023-10-03 23:02:53 -0500

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Nav.css'
 import Logo from '../images/Logo.svg'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 function Nav() {
   return (
@@ -28,8 +29,9 @@ function Nav() {
           </li>
         </ul>
       </nav>
-      
-
+      <btn class={styles.mobile_btn}>
+        <AiOutlineMenu size={25}/>
+      </btn>
     </header>
   )
 }
