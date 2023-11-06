ed7b07500d1893400ba5237131644cc1d5cac601
Amos <amossullivan23@gmail.com>
2023-11-06 15:12:33 -0600

    Hero updates

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,9 +5,14 @@ import styles from './Hero.module.css'
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <form>
-
-      </form>
+      <div className={styles.text}>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>We are a family owned mediterranean resraurant, focused on traditional recipes served with a modern twist.</p>
+      </div>
+      <a className={styles.reserve_btn} href="">
+      <btn>Reserve a Table</btn>
+      </a>
     </div>
   )
 }
