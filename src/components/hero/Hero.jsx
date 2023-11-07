7a65cb2b2608722ac3daabaef545b402d36d6ab6
Amos <amossullivan23@gmail.com>
2023-11-07 15:42:49 -0600

    Hero updates

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Hero.module.css'
+import foodPhoto from '../../images/restauranfood.jpg'
 
 
 const Hero = () => {
@@ -10,9 +11,13 @@ const Hero = () => {
         <h2>Chicago</h2>
         <p>We are a family owned mediterranean resraurant, focused on traditional recipes served with a modern twist.</p>
       </div>
+
       <a className={styles.reserve_btn} href="">
       <btn>Reserve a Table</btn>
       </a>
+
+      <img src={foodPhoto} alt="restaurant food photo" className={styles.overhang_img}/>
+      
     </div>
   )
 }
