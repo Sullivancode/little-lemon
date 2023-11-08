450dc313118709759f1569cf1fb3a6dd0ffa160c
Amos <amossullivan23@gmail.com>
2023-11-08 14:41:35 -0600

    overhaul

--- src/components/special/Special.jsx
+++ src/components/special/Special.jsx
@@ -3,7 +3,21 @@ import styles from './Special.module.css'
 
 function special() {
   return (
-    <div>special</div>
+    <div className={styles.special}>
+
+      <div className={styles.special_text}>
+        <h2>This weeks specials!</h2>
+      </div>
+
+      <a className={styles.online_menu_btn} href="">
+      <btn>Online Menu</btn>
+      </a>
+
+      <div className={styles.card}>
+        
+      </div>
+
+    </div>
   )
 }
 
