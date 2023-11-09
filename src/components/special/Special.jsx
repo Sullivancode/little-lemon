6e89fdf3111d26e817673583afee2308a4c40d5f
Amos <amossullivan23@gmail.com>
2023-11-09 04:43:41 -0600

    special complete

--- src/components/special/Special.jsx
+++ src/components/special/Special.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Special.module.css'
 import Lemond from '../../images/lemon dessert.jpg'
 import Greek from '../../images/greek salad.jpg'
+import Bread from '../../images/bruchetta.svg'
 import {MdDeliveryDining} from 'react-icons/md'
 
 function special() {
@@ -20,17 +21,27 @@ function special() {
         <img src={Greek} alt='Greek Salad'/>
         <div className={styles.card1_container}>
           <h4>Greek salad</h4>
-          <p className={styles.price}>$12.99</p>
+          <p className={styles.price1}>$12.99</p>
           <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
           <a href=""><b>Order online</b> <MdDeliveryDining /></a>
         </div>
       </div>
 
       <div className={styles.card2}>
-        <img src={Lemond} alt='Lemon dessert'/>
+        <img src={Bread} alt='Bruchetta'/>
         <div className={styles.card2_container}>
+          <h4>Bruchetta</h4>
+          <p className={styles.price2}>$5.99</p>
+          <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+          <a href=""><b>Order online</b> <MdDeliveryDining /></a>
+        </div>
+      </div>
+
+      <div className={styles.card3}>
+        <img src={Lemond} alt='Lemon dessert'/>
+        <div className={styles.card3_container}>
           <h4>lemon dessert</h4>
-          <p className={styles.price}>$5.00</p>
+          <p className={styles.price3}>$5.00</p>
           <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
           <a href=""><b>Order online</b> <MdDeliveryDining /></a>
         </div>
