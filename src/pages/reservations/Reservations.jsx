9e50eee8baa0df999b7cbf1b655bb66525cadd2c
Amos <amossullivan23@gmail.com>
2023-12-04 21:43:25 -0600

    radio buttons

--- src/pages/reservations/Reservations.jsx
+++ src/pages/reservations/Reservations.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './Reservations.module.css';
 import Navbar from '../../components/navbar/Navbar'
 
 function Reservations() {
   return (
+    
     <>
     <Navbar />
-    <p>same page boi.</p>
+    <div  className={styles.reservations}>
+      <div className={styles.text}>
+        <h1>Reservations</h1>
+      </div>
+      <div>
+      <label>
+        <input type="radio" name="seatingOption" value="option1" />
+        Indoor seating 
+      </label>
+
+      <label>
+        <input type="radio" name="seatingOption" value="option2" />
+        Outdoor seating
+      </label>
+      </div>
+    </div>
     </>
   )
 }
